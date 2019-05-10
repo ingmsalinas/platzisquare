ddbf54028ab3850fafe5fbebfab953dbd56f922f
msalinas <mario.salinas.arenas@gmail.com>
2019-05-10 17:10:45 -0500

    v0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgModule, ApplicationRef, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
+import { ResaltarDirective } from './directives/resaltar.directive';
+import { ContarClicksDirective } from './directives/contar-clicks.directive';
 
+const appRoutes: Routes = [
+  {path: '', Component: AppComponent},
+  {path: 'lugares', Component: AppComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResaltarDirective,
+    ContarClicksDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCYzqCWMQeXHVvqV2AzyH0V3XhqubE2fmo'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
